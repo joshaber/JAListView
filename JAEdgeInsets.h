c19c1191924d0a0a59f1814e8bd7bebff74cf60d
joshaber <joshaber@gmail.com>
2011-03-12 16:09:30 -0800

    added a zero inset const

--- JAEdgeInsets.h
+++ JAEdgeInsets.h
@@ -1,8 +1,9 @@
 /*
  *  JAEdgeInsets.h
+ *  GitHub
  *
  *  Created by Josh Abernathy on 1/26/11.
- *  Copyright 2011 Josh Abernathy. All rights reserved.
+ *  Copyright 2011 GitHub. All rights reserved.
  *
  */
 
@@ -13,3 +14,5 @@ typedef struct {
 static inline JAEdgeInsets JAEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right) {
 	return (JAEdgeInsets) { .top = top, .left = left, .bottom = bottom, .right = right };
 }
+
+static const JAEdgeInsets JAEdgeInsetsZero = { .top = 0.0f, .left = 0.0f, .bottom = 0.0f, .right = 0.0f };
